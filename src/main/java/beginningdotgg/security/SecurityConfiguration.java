0ad8e94c6494d7fe7c0fc72cb932ad822ec0a84a
Samuel <1605602@claurendeau.qc.ca>
2025-11-08 09:32:41 -0500

    fix I guess

--- src/main/java/beginningdotgg/security/SecurityConfiguration.java
+++ src/main/java/beginningdotgg/security/SecurityConfiguration.java
@@ -70,7 +70,10 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://api.beginning.gg/"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "https://beginning.gg",
+                "https://api.beginning.gg"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Accept", "Range"));
         configuration.setAllowCredentials(true);
