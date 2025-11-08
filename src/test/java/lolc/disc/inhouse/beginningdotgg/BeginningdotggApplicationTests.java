a927d511a2e8c69b993faa57aff1fe0da8b340ee
Samuel <1605602@claurendeau.qc.ca>
2025-11-08 06:43:26 -0500

    Built

--- src/test/java/lolc/disc/inhouse/beginningdotgg/BeginningdotggApplicationTests.java
+++ src/test/java/lolc/disc/inhouse/beginningdotgg/BeginningdotggApplicationTests.java
@@ -1,13 +1,13 @@
-package lolc.disc.inhouse.beginningdotgg;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BeginningdotggApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package lolc.disc.inhouse.beginningdotgg;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BeginningdotggApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
